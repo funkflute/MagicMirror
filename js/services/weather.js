7eff82f030bb2a8844f01d31e5d05831db96e022
John Hopper <jbhopper@optonline.net>
2016-01-25 00:49:03 -0500

    added openweather API key

--- js/services/weather.js
+++ js/services/weather.js
@@ -101,7 +101,7 @@ app.service('weatherService',function() {
     // update our scope with weather
     this.update = function($scope,$http) {
         // get weather
-        $http.get('http://api.openweathermap.org/data/2.5/forecast/daily?units=imperial&cnt=5&q=' + $scope.options.location)
+        $http.get('http://api.openweathermap.org/data/2.5/forecast/daily?APPID=' + $scope.options.weather_api_key + '&units=imperial&cnt=5&q=' + $scope.options.location)
             .success(function(data) {
 	            if (data.list) {
 	                // get today's weather
